8af08dfc2603f0abde93c55a9bb18cd993087406
Jimmy Hogoboom <jimmyhogoboom@gmail.com>
2024-04-01 17:19:02 -0500

    add footer with links

---
+++ src/components/Copyleft.svelte
@@ -0,0 +1,8 @@
+<span class="copyleft">&copy;</span>
+
+<style lang="scss">
+  .copyleft {
+    display: inline-block;
+    transform: rotate(180deg);
+  }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,6 +5,7 @@
   import Timer from '$components/Timer.svelte';
   import EntryListItem from '$components/EntryListItem.svelte';
   import EntryEdit from '$components/EntryEdit.svelte';
+  import Copyleft from '$components/Copyleft.svelte';
 
   const { findOpenEntry, addOrUpdate, deleteEntry } = Entries;
 
@@ -36,7 +37,7 @@
   const handleStartClick = () => {
     const isClosed = currentEntry && !!currentEntry.endTime;
 
-    const arg: NewTimeEntry | TimeEntryUpdate | undefined = isClosed
+    const arg: NewTimeEntry | TimeEntryUpdate = isClosed
       ? ({ ...currentEntry, endTime: undefined } as TimeEntryUpdate) // Closed task will be re-opened (endTime removed)
       : {
           ...currentEntry,
@@ -111,8 +112,8 @@
   };
 </script>
 
-<div class="full background-dark">
-  <div class="head-wrapper">
+<div class="full background-dark wrapper">
+  <header class="head-wrapper">
     <div class="timer">
       <Timer
         entry={currentEntry}
@@ -121,30 +122,50 @@
         onTitleChange={handleTitleChange(currentEntry)}
       />
     </div>
-  </div>
+  </header>
 
-  <div class="wrapper {open && 'open'}">
-    <div class="list-container">
-      <ul>
-        {#if $entries.length < 1}
-          <div class="prompt">Hit the start button to start a new entry</div>
-        {/if}
-        {#each $entries as entry}
-          {@const isSelected = selectedEntry?.id === entry.id}
-          <EntryListItem {entry} onClick={handleEntryClick(entry)} selected={isSelected} />
-          <div class="popout {isSelected && 'open'}">
-            <EntryEdit
-              {entry}
-              onCancelClick={handleEntryClick(entry)}
-              onCopyClick={handleCopyClick(entry)}
-              onDeleteClick={handleDeleteClick(entry)}
-              onChange={handleChange(entry)}
-            />
-          </div>
-        {/each}
-      </ul>
+  <div class="page-body">
+    <div class="list-wrapper {open && 'open'}">
+      <div class="list-container">
+        <ul>
+          {#if $entries.length < 1}
+            <div class="prompt">Hit the start button to start a new entry</div>
+          {/if}
+          {#each $entries as entry}
+            {@const isSelected = selectedEntry?.id === entry.id}
+            <EntryListItem {entry} onClick={handleEntryClick(entry)} selected={isSelected} />
+            <div class="popout {isSelected && 'open'}">
+              <EntryEdit
+                {entry}
+                onCancelClick={handleEntryClick(entry)}
+                onCopyClick={handleCopyClick(entry)}
+                onDeleteClick={handleDeleteClick(entry)}
+                onChange={handleChange(entry)}
+              />
+            </div>
+          {/each}
+        </ul>
+      </div>
     </div>
   </div>
+
+  <footer>
+    <div class="footer-wrapper">
+      <div>
+        <Copyleft />
+        <a href="https://hogoboom.dev" target="_blank">Jimmy Hogoboom</a>
+      </div>
+      <div>
+        <a href="https://github.com/jimmyhogoboom/tempo-web" target="_blank">
+          <img
+            class="github"
+            src="data:image/svg+xml,%3csvg%20width='98'%20height='96'%20xmlns='http://www.w3.org/2000/svg'%3e%3cpath%20fill-rule='evenodd'%20clip-rule='evenodd'%20d='M48.854%200C21.839%200%200%2022%200%2049.217c0%2021.756%2013.993%2040.172%2033.405%2046.69%202.427.49%203.316-1.059%203.316-2.362%200-1.141-.08-5.052-.08-9.127-13.59%202.934-16.42-5.867-16.42-5.867-2.184-5.704-5.42-7.17-5.42-7.17-4.448-3.015.324-3.015.324-3.015%204.934.326%207.523%205.052%207.523%205.052%204.367%207.496%2011.404%205.378%2014.235%204.074.404-3.178%201.699-5.378%203.074-6.6-10.839-1.141-22.243-5.378-22.243-24.283%200-5.378%201.94-9.778%205.014-13.2-.485-1.222-2.184-6.275.486-13.038%200%200%204.125-1.304%2013.426%205.052a46.97%2046.97%200%200%201%2012.214-1.63c4.125%200%208.33.571%2012.213%201.63%209.302-6.356%2013.427-5.052%2013.427-5.052%202.67%206.763.97%2011.816.485%2013.038%203.155%203.422%205.015%207.822%205.015%2013.2%200%2018.905-11.404%2023.06-22.324%2024.283%201.78%201.548%203.316%204.481%203.316%209.126%200%206.6-.08%2011.897-.08%2013.526%200%201.304.89%202.853%203.316%202.364%2019.412-6.52%2033.405-24.935%2033.405-46.691C97.707%2022%2075.788%200%2048.854%200z'%20fill='%2324292f'/%3e%3c/svg%3e"
+            alt="Github Logo"
+          />
+        </a>
+      </div>
+    </div>
+  </footer>
 </div>
 
 <style lang="scss">
@@ -167,11 +188,12 @@
     z-index: 10;
   }
 
-  .wrapper {
+  .list-wrapper {
     position: relative;
     display: flex;
     flex-direction: column;
     margin: 0 auto;
+    margin-bottom: 2rem;
     max-width: 800px;
     z-index: 1;
 
@@ -209,5 +231,59 @@
   .background-dark {
     background-color: colors.$background-dark;
     color: colors.$text-dim;
+  }
+
+  .wrapper {
+    min-height: 100%;
+    display: flex;
+    flex-direction: column;
+  }
+
+  header,
+  footer {
+    flex-grow: 0;
+    flex-shrink: 0;
+  }
+
+  .page-body {
+    flex-grow: 1;
+  }
+
+  footer {
+    padding: 4rem 0;
+    background: darken(colors.$surface-100, 10);
+    color: inherit;
+    font-family: sans-serif;
+
+    & .footer-wrapper {
+      display: flex;
+      justify-content: space-between;
+      align-content: center;
+      margin: 0 auto;
+      max-width: 800px;
+
+      @media screen and (max-width: variables.$small) {
+        padding: 2rem;
+      }
+    }
+  }
+
+  a {
+    color: inherit;
+    text-underline-offset: 0.1rem;
+
+    &:hover,
+    &:hover:visited {
+      color: colors.$text;
+    }
+
+    &:visited {
+      color: colors.$surface-400;
+    }
+  }
+
+  .github {
+    max-width: 3rem;
+    filter: brightness(0) saturate(100%) contrast(50%);
   }
 </style>
