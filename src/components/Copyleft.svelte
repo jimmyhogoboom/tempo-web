ce3dfd6408f2909f802c3ab93176a976eedfdace
Jimmy Hogoboom <jimmyhogoboom@gmail.com>
2024-04-02 12:05:38 -0500

    add link to info about copyleft

--- src/components/Copyleft.svelte
+++ src/components/Copyleft.svelte
@@ -1,8 +1,11 @@
-<span class="copyleft">&copy;</span>
+<a href="https://copyleft.org/" class="no-link-style"><span class="copyleft">&copy;</span></a>
 
 <style lang="scss">
   .copyleft {
     display: inline-block;
     transform: rotate(180deg);
+  }
+  .no-link-style {
+    color: inherit;
   }
 </style>
