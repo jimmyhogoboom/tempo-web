816d3385ee154cbf908e6d9455773288f49a298a
Jimmy Hogoboom <jimmyhogoboom@gmail.com>
2024-11-20 15:59:09 -0600

    refactor: drop redundant types

--- src/stores/entries.ts
+++ src/stores/entries.ts
@@ -1,10 +1,6 @@
-import Result from 'true-myth/result';
 import Maybe from 'true-myth/maybe';
-import Entries from '$lib/Entries';
+import Entries, { type AddEntryResult, type UpdateEntryResult } from '$lib/Entries';
 import { createLocalStorageStore } from '$stores/stores';
-
-export type UpdateResult = Result<TimeEntry, string>;
-export type AddResult = Result<OpenTimeEntry, string>;
 
 // An interface for the front end to access long-term storage
 // which consolidates that access to multiple storage methods
@@ -15,10 +11,10 @@ export const entries = {
   findOpenEntry: (): Maybe<OpenTimeEntry> => {
     return entriesStorage.findOpenEntry();
   },
-  update: (entryUpdate: TimeEntryUpdate): UpdateResult => {
+  update: (entryUpdate: TimeEntryUpdate): UpdateEntryResult => {
     return entriesStorage.updateEntry(entryUpdate);
   },
-  add: (newEntry: NewTimeEntry): AddResult => {
+  add: (newEntry: NewTimeEntry): AddEntryResult => {
     return entriesStorage.addEntry(newEntry);
   },
 };
